e0a036adc5e816faafe55f92816ef3f47d9771c1
Peter Benavides <peter.r.benavides@gmail.com>
2015-08-31 16:23:24 -0700

    Added styling to files

---
+++ assets/js/animations.js
@@ -0,0 +1,73 @@
+(function(){
+  if (typeof PersonalSite === "undefined") {
+    var PersonalSite = window.PersonalSite = {};
+  }
+
+  AnimationController = PersonalSite.AnimationController = function () {
+    this.bindHandlers();
+  };
+
+  AnimationController.prototype.bindHandlers = function () {
+    $('#works-link').click(this.scrollToWorks.bind(this));
+    $('#home-link').click(this.scrollToHome.bind(this));
+    $('#about-link').click(this.scrollToAbout.bind(this));
+    $('#brand-link').click(this.scrollToHome.bind(this));
+  };
+
+  AnimationController.prototype.scrollToWorks = function (event) {
+    event.preventDefault();
+    if (this.scrolling) {return;}
+    this.scrolling = true;
+    $('.active').removeClass('active');
+    $('#works-link').parent('li').addClass('active');
+    $('html,body').animate(
+      {
+        scrollTop: $("#works").offset().top
+      },
+      {
+        duration: 'slow',
+        complete: function () {
+          this.scrolling = false;
+        }.bind(this)
+      }
+    );
+  };
+
+  AnimationController.prototype.scrollToHome = function (event) {
+    event.preventDefault();
+    if (this.scrolling) {return;}
+    this.scrolling = true;
+    $('.active').removeClass('active');
+    $('#home-link').parent('li').addClass('active');
+    $('html,body').animate(
+      {
+        scrollTop: $("body").offset().top
+      },
+      {
+        duration: 'slow',
+        complete: function () {
+          this.scrolling = false;
+        }.bind(this)
+      }
+    );
+  };
+
+  AnimationController.prototype.scrollToAbout = function (event) {
+    event.preventDefault();
+    if (this.scrolling) {return;}
+    this.scrolling = true;
+    $('.active').removeClass('active');
+    $('#about-link').parent('li').addClass('active');
+    $('html,body').animate(
+      {
+        scrollTop: $("body").offset().top
+      },
+      {
+        duration: 'slow',
+        complete: function () {
+          this.scrolling = false;
+        }.bind(this)
+      }
+    );
+  };
+})();
