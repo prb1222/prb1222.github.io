65c7cf491f7515c89de89d470d3cfda6d4e82b50
Peter Benavides <peter.r.benavides@gmail.com>
2015-11-10 15:48:58 -0800

    Updated google_places

--- google_places/js/app.js
+++ google_places/js/app.js
@@ -4,14 +4,25 @@ window.GooglePlaces = {
   Views: {},
   Routers: {},
   initialize: function() {
+    // Checks to see if geolocation is available
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
+          // This function is asynchronous, and will update the search service
+          // if and when the user shares their location.
           GooglePlaces.userLocation = [position.coords.latitude, position.coords.longitude];
+          GooglePlaces.map = new google.maps.Map(document.getElementById('map'), {
+              center: GooglePlaces.userLocation,
+              zoom: 15
+          });
+          GooglePlaces.service = new google.maps.places.PlacesService(GooglePlaces.map);
         });
     } else {
-        alert("Geolocation is not supported by this browser. Defaulting location");
+        alert("Geolocation is not supported by this browser. Defaulting location...");
     }
 
+    // This defaults the location until the callback gets the user location
+    // The search code references GooglePlaces.service which will point to the default
+    // location before the callback, and the new location afterwards.
     GooglePlaces.userLocation = new google.maps.LatLng(-33.8665433, 151.1956316);
     GooglePlaces.map = new google.maps.Map(document.getElementById('map'), {
         center: GooglePlaces.userLocation,
--- google_places/js/routers/router.js
+++ google_places/js/routers/router.js
@@ -5,15 +5,18 @@ GooglePlaces.Routers.Router = Backbone.Router.extend({
 
   routes: {
     "": "index",
-    "posts/:id": "postShow"
   },
 
   index: function () {
+    // The collection here is not used, but it is easier to refactor given that
+    // there already exists a collection. For instance, we can add a comparator
+    // to the collection which will allow ordering by price.
     this.placesCollection = this.placesCollection || new GooglePlaces.Collections.Places();
     var view = new GooglePlaces.Views.PlacesIndex({collection: this.placesCollection});
     this.swapView(view);
   },
 
+  // This is unnecessary as there is only one major view. However, this is easier to expand upon
   swapView: function(view) {
     this._view && this._view.remove();
     this._view = view;
