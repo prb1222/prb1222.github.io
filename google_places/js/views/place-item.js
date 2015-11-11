9d54ad8189260e0d0135a0e606323d96b51798dd
Peter Benavides <peter.r.benavides@gmail.com>
2015-11-10 16:11:58 -0800

    Updated google_places with logo

--- google_places/js/views/place-item.js
+++ google_places/js/views/place-item.js
@@ -5,10 +5,6 @@ GooglePlaces.Views.PlaceItem = Backbone.View.extend({
 
   className: "index-item-view",
 
-  initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
-  },
-
   render: function () {
     this.$el.html(this.template({place: this.model}));
     return this;
